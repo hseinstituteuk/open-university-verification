43053cfa4169e4c3ce0a8a1c3976d40bf2f26c14
HSE Institute UK <abodxxx925@gmail.com>
2025-07-30 08:48:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const params = new URLSearchParams(window.location.search);
-  const certFromURL = params.get("cert");
+  const certFromURL = params.get("OUK-BCOM-2005-1147");
   if (certFromURL) {
     document.getElementById("certInput").value = certFromURL;
     verifyCert(certFromURL);
